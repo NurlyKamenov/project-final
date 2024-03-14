32575ddf9563dfcf3240d1ca52af880ecbcc856b
NurlyKamenov <nurly1983@gmail.com>
2024-03-14 17:00:15 +0600

    5.Test get and update

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,68 @@
 package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
+import com.javarush.jira.bugtracking.project.ProjectRepository;
+import com.javarush.jira.profile.internal.ProfileRepository;
+import com.javarush.jira.profile.internal.model.Profile;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.javarush.jira.common.util.JsonUtil.writeValue;
+import static com.javarush.jira.profile.internal.web.ProfileRestController.REST_URL;
+import static com.javarush.jira.login.internal.web.UserTestData.*;
+import static com.javarush.jira.profile.internal.web.ProfileTestData.*;
+import static com.javarush.jira.project.internal.web.ProjectTestData.projectTo2;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 class ProfileRestControllerTest extends AbstractControllerTest {
+    @Autowired
+    private ProfileRepository profileRepository;
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value("1"));
+    }
+    @Test
+    void getUnAuth() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void update() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(getUpdatedTo())))
+                .andDo(print())
+                .andExpect(status().isNoContent());;
+
+        Profile profile = profileRepository.getOrCreate(user.id());
+        PROFILE_MATCHER.assertMatch(profile, getUpdated(user.id()));
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void getNewTo() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(ProfileTestData.getNewTo())))
+                .andDo(print())
+                .andExpect(status().isNoContent());;
+
+        Profile profile = profileRepository.getOrCreate(user.id());
+        PROFILE_MATCHER.assertMatch(profile, getNew(user.id()));
+    }
+
+
 
 }
